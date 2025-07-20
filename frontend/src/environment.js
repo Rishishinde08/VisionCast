9ffb770d45e355acf930a5cd841236cc364b73a7
Rishishinde08 <rishikeshs2003@gmail.com>
2025-07-20 13:11:47 +0530

    Production Phase

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,18 @@
-// let IS_PROD = true;
-const server  = "http://localhost:8000";
+// // let IS_PROD = true;
+// const server  =  "http://localhost:8000";
+
+// export default server;
+
+
+
+
+// environment.js
+
+// Toggle this to `true` for production (Render), `false` for local development
+const IS_PROD = true;
+
+const server = IS_PROD
+  ? "https://visioncast-rishi.onrender.com"   // Render deployment URL
+  : "http://localhost:8000";                  // Localhost for dev
 
 export default server;
